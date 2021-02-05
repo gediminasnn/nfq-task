f56ea4d77e2cffe6ca6b5491f184c6f690267426
gediminasnn <neniusgediminas@gmail.com>
2021-02-05 16:38:39 +0200

    edit

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -11,7 +11,7 @@ class CustomerController extends AbstractController
     /**
      * @Route("/new", name="new_reservation_panel")
      */
-    public function newReservationPanel(): Response
+    public function newReservationPanele(): Response
     {
         return $this->render('customer/index.html.twig');
     }
