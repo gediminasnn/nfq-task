4b11f6def5fcded415d763be3b173dc879977bdf
gediminasnn <neniusgediminas@gmail.com>
2021-02-08 11:21:10 +0200

    Created first test

---
+++ tests/ReservationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ReservationTest extends TestCase
+{
+    public function test()
+    {
+        $actual = true;
+        $this->assertEquals(true,$actual);
+    }
+}
