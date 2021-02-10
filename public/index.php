bfbe772f4eda68d38a5c32fa7bf981fc29fb2d57
gediminasnn <neniusgediminas@gmail.com>
2021-02-10 16:46:15 +0200

    added Lithuania's timezone to the project

--- public/index.php
+++ public/index.php
@@ -16,6 +16,7 @@ if ($_SERVER['APP_DEBUG']) {
 }
 
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+date_default_timezone_set( 'Europe/Vilnius' );
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
