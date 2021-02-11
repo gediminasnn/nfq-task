8bc68a2563c0509d1675ceb990f860f3d968ef60
gediminasnn <neniusgediminas@gmail.com>
2021-02-11 15:25:05 +0200

    moved findReservationQueuePosition method to ReservationService.php and tagged unused methods

--- src/Service/Reservation/ReservationService.php
+++ src/Service/Reservation/ReservationService.php
@@ -6,16 +6,13 @@ namespace App\Service\Reservation;
 
 use App\Entity\Reservation;
 use App\Repository\ReservationRepository;
-use Doctrine\ORM\EntityManagerInterface;
 
 class ReservationService
 {
-    private $entityManager;
     private $reservationRepository;
 
-    public function __construct(EntityManagerInterface $entityManager, ReservationRepository $reservationRepository)
+    public function __construct(ReservationRepository $reservationRepository)
     {
-        $this->entityManager = $entityManager;
         $this->reservationRepository = $reservationRepository;
     }
 
@@ -35,29 +32,42 @@ class ReservationService
         return $doesBegunReservationExist;
     }
 
+//unused method
+//    public function updatePastReservations(): void
+//    {
+//        $qb = $this->reservationRepository->createQueryBuilder('r');
+//
+//        $reservations =
+//            $qb->where('r.state = ?1')
+//                ->orwhere('r.state = ?2')
+//                ->andWhere('r.endTime < :now')
+//                ->setParameters(['now' => date("Y-m-d H:i:s"), 1 => 'pending', 2 => 'begun'])
+//                ->orderBy('r.startTime', 'ASC')
+//                ->getQuery()
+//                ->getResult();
+//
+//        foreach ($reservations as $reservation) {
+//            if ($reservation->getState() === "begun") {
+//                $reservation->setState("ended");
+//            } else if ($reservation->getState() === "pending") {
+//                $reservation->setState("canceled");
+//            }
+//            $this->entityManager->persist($reservation);
+//        }
+//        $this->entityManager->flush();
+//    }
 
-    public function updatePastReservations(): void
+    public function findReservationQueuePosition(Reservation $reservation): ?int
     {
-        $qb = $this->reservationRepository->createQueryBuilder('r');
-
-        $reservations =
-            $qb->where('r.state = ?1')
-                ->orwhere('r.state = ?2')
-                ->andWhere('r.endTime < :now')
-                ->setParameters(['now' => date("Y-m-d H:i:s"), 1 => 'pending', 2 => 'begun'])
-                ->orderBy('r.startTime', 'ASC')
-                ->getQuery()
-                ->getResult();
-
-        foreach ($reservations as $reservation) {
-            if ($reservation->getState() === "begun") {
-                $reservation->setState("ended");
-            } else if ($reservation->getState() === "pending") {
-                $reservation->setState("canceled");
+        $result = null;
+        $reservations = $this->reservationRepository->findAllUpcomingPendingReservationsBySpecialist($reservation->getSpecialist());
+        $count = count($reservations);
+        for ($i = 1; $i <= $count; $i++) {
+            if ($reservations[$i - 1]->getCode() === $reservation->getCode()) {
+                $result = $i;
+                break;
             }
-            $this->entityManager->persist($reservation);
         }
-        $this->entityManager->flush();
+        return $result;
     }
-
 }
